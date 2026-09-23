64dfe627a09d320ace4b585d019d66b3fa254e35
Olusola Adebayo <beresdowds@gmail.com>
2026-09-23 14:25:34 +0100

    Change email API endpoint to staging URL
    Update API endpoint for sending outbound emails.

--- src/components/admin/EmailProviderHealthCard.tsx
+++ src/components/admin/EmailProviderHealthCard.tsx
@@ -106,7 +106,7 @@ export function EmailProviderHealthCard() {
 
     setSendingTest(true);
     try {
-      const res = await fetch("/api/functions/send-outbound-email", {
+      https://staging.rentmaikar.com/api/functions/send-outbound-email
   method: "POST",
   headers: {
     "Content-Type": "application/json",
